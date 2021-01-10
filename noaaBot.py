637832660f4f3a09a6d83a2ecb7e89931ed3549d
mrchhaya <mohitchhaya24@gmail.com>
2021-01-10 12:30:01 -0500

    Update Bot
    Added AT, Wind, WL, and Pressure capabilities. Changed search method to use regular expressions

--- noaaBot.py
+++ noaaBot.py
@@ -1,15 +1,17 @@
-import discord,os,time,json,random
+import discord,os,time,json,random, re
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 client = discord.Client()
 load_dotenv('secrets.env')
 discordToken = os.getenv('DISCORD_TOKEN')
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 numberRefDict,nameRefDict = {},{}
 
 @client.event
@@ -22,17 +24,62 @@ async def on_ready():
         nameRefDict = json.load(f)
 
 def webScrape(url):
-    print(url)
+
+    wT, aT, wind, wL, aP = False, False, False, False, False
+    returnList = ['Could not find','Could not find','Could not find','Could not find','Could not find']
+
     driver=webdriver.Chrome(chrome_options=chrome_options)
     driver.get(url)
     try:
-        wTempElement = WebDriverWait(driver,15).until(EC.presence_of_element_located((By.XPATH, "//div[@id='wttext']/h1")))
-        wTemp = driver.find_element_by_xpath("//div[@id='wttext']/h1").get_attribute('innerHTML')
-        driver.quit()
-        return wTemp
+        sensorTable = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//div[@class = 'span12']/table/tbody")))
     except TimeoutException:
         driver.quit()
-        return "Couldn't find watertemp!"
+        return returnList
+
+    for tr in sensorTable.find_elements_by_xpath('.//tr'):
+        if tr.find_element_by_xpath('.//td').get_attribute('innerHTML').lower() == "water temperature":
+            wT = True
+        if tr.find_element_by_xpath('.//td').get_attribute('innerHTML').lower() == "air temperature":
+            aT = True
+        if tr.find_element_by_xpath('.//td').get_attribute('innerHTML').lower() == "wind":
+            wind = True
+        if tr.find_element_by_xpath('.//td').get_attribute('innerHTML').lower() == "acoustic wl":
+            wL = True
+        if tr.find_element_by_xpath('.//td').get_attribute('innerHTML').lower() == "barometric pressure":
+            aP = True
+    try:     
+        if wT:
+            wTemp = driver.find_element_by_xpath("//div[@id='wttext']/h1").get_attribute('innerHTML')
+            returnList[0] =  wTemp
+    except NoSuchElementException:
+        pass
+    try:
+        if aT:
+            aTemp = driver.find_element_by_xpath("//div[@id='attext']/h1").get_attribute('innerHTML')
+            returnList[1] =  aTemp
+    except NoSuchElementException:
+        pass
+    try:
+        if wind:
+            windSpeed = driver.find_element_by_xpath("//div[@id='windtext']/h1").get_attribute('innerHTML')
+            returnList[2] =  windSpeed
+    except NoSuchElementException:
+        pass
+    try:
+        if wL:
+            wLevel = driver.find_element_by_xpath("//div[@id='wltext']/h1").get_attribute('innerHTML')
+            returnList[3] =  wLevel
+    except NoSuchElementException:
+        pass
+    try:
+        if aP:
+            airPress = driver.find_element_by_xpath("//div[@id='barotext']/h1").get_attribute('innerHTML')
+            returnList[4] =  airPress
+    except NoSuchElementException:
+        pass
+
+    driver.quit()
+    return returnList
 
 @client.event
 async def on_message(message):
@@ -44,23 +91,29 @@ async def on_message(message):
         startT = time.time()
         params = message.content.split(" ")
         if any(i.isdigit() for i in params[-1]):
-            print(numberRefDict)
             if params[-1] in numberRefDict:
-                await message.channel.send("Water Temperature for " + numberRefDict[params[-1]].get('name') + ": " + webScrape(numberRefDict.get(params[-1]).get('link')))
+                infoEmbed = discord.Embed(title = numberRefDict[params[-1]].get('name'), color = int(hex(random.randint(0,16777215)),16))
+                infoList = webScrape(numberRefDict.get(params[-1]).get('link'))
+                infoEmbed.add_field(name = 'Water Temperature', value = infoList[0])
+                infoEmbed.add_field(name = 'Air Temperature', value = infoList[1])
+                infoEmbed.add_field(name = 'Wind Speed', value = infoList[2])
+                infoEmbed.add_field(name = 'Water Level', value = infoList[3])
+                infoEmbed.add_field(name = 'Air Pressure', value = infoList[4] + ' millibars')
+                await message.channel.send(embed = infoEmbed)
                 print(time.time()-startT)
-
-
-            # if int(params[-1]) == 1:
-            #     await message.channel.send(webScrape(numberRefDict.get(params[-1])))
-            #     print(time.time()-startT)
-            # elif int(params[-1]) == 2:
-            #     await message.channel.send(webScrape(numberRefDict.get(params[-1])))
-        # elif isinstance(params[-1], str):
-        #     print("name")
-        #     print(params[-1])
-        #     if params[-1].lower() == "Washington Channel".lower():
-        #         await message.channel.send(webScrape(nameRefDict.get(params[-1])))
-        #         print(time.time()-startT)
+        elif isinstance(params[-1], str):
+            for i in numberRefDict:
+                if re.search(r"\b" + re.escape(" ".join(params[1:]).lower()) + r"\b", numberRefDict[i].get('name').lower()):
+                    infoEmbed = discord.Embed(title = numberRefDict[i].get('name'), color = int(hex(random.randint(0,16777215)),16))
+                    infoList = webScrape(numberRefDict[i].get('link'))
+                    infoEmbed.add_field(name = 'Water Temperature', value = infoList[0])
+                    infoEmbed.add_field(name = 'Air Temperature', value = infoList[1])
+                    infoEmbed.add_field(name = 'Wind Speed', value = infoList[2])
+                    infoEmbed.add_field(name = 'Water Level', value = infoList[3])
+                    infoEmbed.add_field(name = 'Air Pressure', value = infoList[4] + ' millibars')
+                    await message.channel.send(embed = infoEmbed)
+                    print(time.time()-startT)
+                    break
         else:
             await message.channel.send("Weird Params")
 
@@ -76,40 +129,15 @@ async def on_message(message):
             numberRefDict.update(y)
             print(numberRefDict)
 
-            await message.channel.send("Added!")
+            await message.channel.send("Added " + " ".join(splitMessage[2:]) +"!")
 
             with open("numref.json", 'w') as f:
                 json.dump(numberRefDict, f)
         
-        # y = {int(lastNum)+1:message.content.split(" ")[-1]}
-
-        # print(message.content.split(" ")[-1])
-
-        # numberRefDict.update(y)
-
-        # await message.channel.send("Added!")
-
-        # with open("numref.json", 'w') as f:
-        #     json.dump(numberRefDict,f)
-
     if message.content.startswith('$help'):
         helpEmbed = discord.Embed(title='Commands:', color = int(hex(random.randint(0,16777215)),16))      
         for i in numberRefDict.keys():
             helpEmbed.add_field(name = "Location: " + numberRefDict[i].get('name'), value = "Index: " + str(i))
         await message.channel.send(embed = helpEmbed)
-
-
-
-        # if message.content[1:].lower() == "temp":
-        #     startTime = time.time()
-        #     driver=webdriver.Chrome(chrome_options=chrome_options)
-        #     driver.get("https://tidesandcurrents.noaa.gov/stationhome.html?id=8594900")
-
-        #     driver.quit()
-        #     print(time.time()-startTime)
-        #     await message.channel.send(wTemp)
-
-
-        # await message.channel.send('Hello!')
 
 client.run(discordToken)
